d71961fa923dfc86361d13c587b7f6f68aa8f6fc
Ftih07 <naufalfathi37@gmail.com>
2025-07-13 15:31:37 +0700

    Fix Routing Business Matching

--- routes/web.php
+++ routes/web.php
@@ -6,5 +6,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [NavigationController::class, 'home'])->name('home');
 Route::get('/about', [NavigationController::class, 'about'])->name('about');
 Route::get('/conference', [NavigationController::class, 'conference'])->name('conference');
-Route::get('/businessmatching', [NavigationController::class, 'businessmatching'])->name('businessmatching');
+Route::get('/business-matching', [NavigationController::class, 'businessmatching'])->name('businessmatching');
 Route::get('/exhibition', [NavigationController::class, 'exhibition'])->name('exhibition');
